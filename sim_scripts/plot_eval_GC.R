32ae943baba9fe04bd29c583f4342081c75dfd5e
tiffttu <tiffany.tu@duke.edu>
2025-04-19 08:33:09 -0400

    update plotting scripts

--- sim_scripts/plot_eval_GC.R
+++ sim_scripts/plot_eval_GC.R
@@ -31,25 +31,13 @@ main = df_long %>%
                                      ifelse(metric %in% c("srmsd_vals", "srmsd_vals_gc"), "srmsd", metric)))) %>% 
   filter(!(analysis %in% c("S1", "S2", "S3", "male", "female")) & !(metric %in% c( "infl_pvals_null"))) 
 
-main$analysis <- factor(main$analysis, 
-                           levels = c("joint_pvals", "sex-meta_pvals", "sex-meta_pvals_gc", "sex-meta_pval_gc_null", 
-                                      "subpop-meta_pvals", "subpop-meta_pvals_gc", "subpop-meta_pval_gc_null"))
-# SRMSD and SRMSD GC
-# Combine dark and light colors
-# custom_colors  <- c(
-#   "#BC3C29",  # Dark Red
-#   "#F4CCCC",  # Light Red
-#   "#0072B5",  # Dark Blue
-#   "#B3D4F5",  # Light Blue
-#   "#E18727",  # Dark Orange
-#   "#FAD7A0"   # Light Orange
 
 # regroup for facet wrap
 main_grouped <- main %>%
   mutate(row_group = case_when(
     metric %in% c("auc_vals", "auc_vals_gc") ~ "AUC",
     metric %in% c("infl_pvals", "infl_pvals_gc") ~ "Inflation Factor",
-    metric %in% c("srmsd_vals", "srmsd_vals_gc") ~ "SRMSD",
+    metric %in% c("srmsd_vals", "srmsd_vals_gc") ~ "SRMSDp",
     TRUE ~ metric
   )) %>%
   mutate(metric_short = case_when(
@@ -61,22 +49,34 @@ main_grouped <- main %>%
   )
 
 facet_title_color <- "#D3D3D380"
+lines_df <- data.frame(
+  yintercept = c(0.95, 1.05, -0.01, 0.01),
+  row_group = c("Inflation Factor", "Inflation Factor", "SRMSDp", "SRMSDp")
+)
 
 png( '/hpc/group/ochoalab/tt207/meta_analysis_aim/plots/main_analyses_binary_gc.png', width=14, height=20, res=300, units = 'in')
 ggplot(main_grouped , aes(x = source, y = value, fill = analysis)) +
+  geom_hline(data = lines_df, aes(yintercept = yintercept), color = "gray", linewidth = 1.5, linetype = "dashed") +
   geom_boxplot(width = 0.6, alpha = 0.5, size = 1.2) +
-  labs(x = "Simulation", y = "Value", fill = "Subset") + theme_bw() + 
-  facet_grid(rows = vars(row_group), cols = vars(metric_short), scales = "free_y") + scale_fill_nejm() + 
-  theme(strip.background = element_rect(fill = facet_title_color, size = 1.5), panel.spacing = unit(1, "lines"), 
-        strip.text = element_text(size = 36),
-        axis.title = element_text(size = 27), # Adjust the legend title font size
-        legend.text = element_text(size = 28), axis.text = element_text(size = 26),
-        legend.position="top", legend.title = element_blank(), legend.key.size = unit(3, "lines"),
-        panel.border = element_rect(linewidth = 1.5), axis.line = element_line(linewidth = 1.5)) +
+  labs(x = "Simulation", y = "Value", fill = "Subset") + 
+  theme_bw() + 
+  facet_grid(rows = vars(row_group), cols = vars(metric_short), scales = "free_y") +
+  scale_fill_nejm() + 
+  theme(
+    strip.background = element_rect(fill = facet_title_color, size = 1.5),
+    panel.spacing = unit(1, "lines"), 
+    strip.text = element_text(size = 36),
+    axis.title = element_text(size = 27),
+    legend.text = element_text(size = 28),
+    axis.text = element_text(size = 26),
+    legend.position = "top",
+    legend.title = element_blank(),
+    legend.key.size = unit(3, "lines"),
+    panel.border = element_rect(linewidth = 1.5),
+    axis.line = element_line(linewidth = 1.5)
+  ) +
   guides(color = guide_legend(override.aes = list(size = 25)))
 invisible( dev.off() )
-
-
 ############### quantitative traits
 sim1 = read.table("sim1_h08_1_20_gc_quant.txt", header = TRUE)
 sim2 = read.table("sim2_h08_1_20_gc_quant.txt", header = TRUE)
@@ -100,7 +100,7 @@ main_grouped <- main %>%
   mutate(row_group = case_when(
     metric %in% c("auc_vals", "auc_vals_gc") ~ "AUC",
     metric %in% c("infl_pvals", "infl_pvals_gc") ~ "Inflation Factor",
-    metric %in% c("srmsd_vals", "srmsd_vals_gc") ~ "SRMSD",
+    metric %in% c("srmsd_vals", "srmsd_vals_gc") ~ "SRMSDp",
     TRUE ~ metric
   )) %>%
   mutate(metric_short = case_when(
@@ -111,21 +111,36 @@ main_grouped <- main %>%
   metric_short = factor(metric_short, levels = c("Original", "GC-corrected"))
   ) %>% 
   filter(
-    (row_group != "Inflation Factor" & row_group != "SRMSD" ) | 
+    (row_group != "Inflation Factor" & row_group != "SRMSDp" ) | 
       (row_group == "Inflation Factor" & value > 0.95 & value < 1.15) |
-      (row_group == "SRMSD" & value > -0.01 & value < 0.03)
+      (row_group == "SRMSDp" & value > -0.01 & value < 0.03)
   )
+
+lines_df <- data.frame(
+  yintercept = c(0.95, 1.05, -0.01, 0.01),
+  row_group = c("Inflation Factor", "Inflation Factor", "SRMSDp", "SRMSDp")
+)
 
 png( '/hpc/group/ochoalab/tt207/meta_analysis_aim/plots/main_analyses_quant_gc.png', width=14, height=20, res=300, units = 'in')
 ggplot(main_grouped , aes(x = source, y = value, fill = analysis)) +
+  geom_hline(data = lines_df, aes(yintercept = yintercept), color = "gray", linewidth = 1.5, linetype = "dashed") +
   geom_boxplot(width = 0.6, alpha = 0.5, size = 1.2) +
-  labs(x = "Simulation", y = "Value", fill = "Subset") + theme_bw() + 
-  facet_grid(rows = vars(row_group), cols = vars(metric_short), scales = "free_y") + scale_fill_nejm() + 
-  theme(strip.background = element_rect(fill = facet_title_color, size = 1.5), panel.spacing = unit(1, "lines"), 
-        strip.text = element_text(size = 36),
-        axis.title = element_text(size = 27), # Adjust the legend title font size
-        legend.text = element_text(size = 28), axis.text = element_text(size = 26),
-        legend.position="top", legend.title = element_blank(), legend.key.size = unit(3, "lines"),
-        panel.border = element_rect(linewidth = 1.5), axis.line = element_line(linewidth = 1.5)) +
+  labs(x = "Simulation", y = "Value", fill = "Subset") + 
+  theme_bw() + 
+  facet_grid(rows = vars(row_group), cols = vars(metric_short), scales = "free_y") +
+  scale_fill_nejm() + 
+  theme(
+    strip.background = element_rect(fill = facet_title_color, size = 1.5),
+    panel.spacing = unit(1, "lines"), 
+    strip.text = element_text(size = 36),
+    axis.title = element_text(size = 27),
+    legend.text = element_text(size = 28),
+    axis.text = element_text(size = 26),
+    legend.position = "top",
+    legend.title = element_blank(),
+    legend.key.size = unit(3, "lines"),
+    panel.border = element_rect(linewidth = 1.5),
+    axis.line = element_line(linewidth = 1.5)
+  ) +
   guides(color = guide_legend(override.aes = list(size = 25)))
 invisible( dev.off() )
